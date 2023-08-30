ddb214f3d481844a604b76f0094db4030b1b841e
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2023-08-30 20:21:56 +0200

    Add hovering effect using mouseover

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const grid = document.getElementById("grid");
-
 /*Create grid*/
 function createGrid(){
     for (let i = 0; i < 16 * 16; i++){
-            const gridElement = document.createElement("div");
-            grid.appendChild(gridElement).className = "gridElement";
+        const gridElement = document.createElement("div");
+        /*Add hovering effect*/
+        gridElement.addEventListener("mouseover", ()  =>{
+            gridElement.style.cssText = "background-color: black;"
+        });
+        grid.appendChild(gridElement).className = "gridElement";
+
     }
 }
 createGrid();

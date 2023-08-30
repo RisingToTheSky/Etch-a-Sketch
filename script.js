2f005614f0b59f3dc6eda0aacf46a79fd953a608
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2023-08-30 21:34:52 +0200

    Add event listener to reset button

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const grid = document.getElementById("grid");
 const eraser = document.getElementById("eraser");
-const clearBoard = document.getElementById("clearBoard");
-const changeGridSize = document.getElementById("changeGridSize");
+const gridReset = document.getElementById("reset");
+const gridSize = document.getElementById("changeGridSize");
+const container = document.getElementById("container");
 /*Create grid*/
 function createGrid(){
     for (let i = 0; i < 16 * 16; i++){
@@ -11,7 +12,16 @@ function createGrid(){
             gridElement.style.cssText = "background-color: black;"
         });
         grid.appendChild(gridElement).className = "gridElement";
-
+        reset.addEventListener("click", () => {
+            gridElement.style.cssText = "background-color: white;"
+        });
     }
 }
 createGrid();
+
+
+
+
+function changeGridSize(){
+
+}

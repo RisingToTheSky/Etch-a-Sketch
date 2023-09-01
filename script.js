3a266a18363f140470737878213c87d584ead151
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2023-09-01 14:00:30 +0200

    Change function to consider size

--- script.js
+++ script.js
@@ -4,24 +4,21 @@ const gridReset = document.getElementById("reset");
 const gridSize = document.getElementById("changeGridSize");
 const container = document.getElementById("container");
 /*Create grid*/
-function createGrid(){
-    for (let i = 0; i < 16 * 16; i++){
-        const gridElement = document.createElement("div");
-        /*Add hovering effect*/
-        gridElement.addEventListener("mouseover", ()  =>{
-            gridElement.style.cssText = "background-color: black;"
-        });
-        grid.appendChild(gridElement).className = "gridElement";
-        reset.addEventListener("click", () => {
-            gridElement.style.cssText = "background-color: white;"
-        });
-    }
-}
-createGrid();
+function createSquare(size) {
+    const gridElement = document.createElement('div');
+    gridElement.classList.add('box');
+    gridElement.style.width = `${size}px`;
+    gridElement.style.height = `${size}px`;
+  
+    return gridElement;
+  }
+
+
+gridReset.addEventListener("click", () => {
+    grid.innerHTML =""
+    defaultGrid();
+});
 
 
 
 
-function changeGridSize(){
-
-}
